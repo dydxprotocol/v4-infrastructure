aa9e713c457641d98f7dfde44511f8f35712cd0a
Will Liu <will@dydx.exchange>
2023-10-04 14:44:42 -0400

    set memory/cpu explicitly for container

--- modules/datadog_agent/output.tf
+++ modules/datadog_agent/output.tf
@@ -12,3 +12,13 @@ output "ecs_ec2_container_volumes" {
   value       = local.ecs_ec2_container_volumes
   description = "container volumes for Datadog sidecar for ECS EC2"
 }
+
+output "datadog_cpu" {
+  value       = var.container_cpu_units
+  description = "The number of cpu units to reserve for the datadog agent container."
+}
+
+output "datadog_memory" {
+  value       = var.container_memory_reservation
+  description = "The amount of memory (in MiB) to reserve for the datadog agent container."
+}
