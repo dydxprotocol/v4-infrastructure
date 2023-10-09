a4db75f0d5a3064b77a78f7fc57ed3825758cdee
Will Liu <will@dydx.exchange>
2023-10-09 11:30:27 -0700

    add monitors

--- modules/indexer_monitors/monitors.tf
+++ modules/indexer_monitors/monitors.tf
@@ -121,7 +121,7 @@ resource "datadog_monitor_json" "orderbook_crossed" {
 EOF
 }
 
-resource "datadog_monitor_json" "last_processed_block" {
+resource "datadog_monitor_json" "last_processed_block_last_30min" {
   monitor = <<EOF
 {
 	"id": 107439681,
@@ -144,6 +144,68 @@ resource "datadog_monitor_json" "last_processed_block" {
 		"include_tags": false,
 		"no_data_timeframe": 60,
 		"new_host_delay": 300,
+		"silenced": {}
+	},
+	"priority": null,
+	"restricted_roles": null
+}
+EOF
+}
+
+resource "datadog_monitor_json" "last_processed_block_last_10min" {
+  monitor = <<EOF
+{
+	"id": 132789180,
+	"name": "[${var.environment}] Indexer Last processed block on Indexer is > 100 blocks behind latest block",
+	"type": "query alert",
+	"query": "min(last_10m):max:dydxprotocol.cometbft_consensus_height{env:${var.environment}} - max:ender.processing_block_height{env:${var.environment},service:indexer} > 100",
+  "message": "${local.monitor_suffix_literal}",
+	"tags": [
+		"team:${var.team}",
+		"env:${var.env_tag}"
+	],
+	"options": {
+		"thresholds": {
+			"critical": 100
+		},
+		"notify_audit": false,
+		"require_full_window": false,
+		"notify_no_data": true,
+		"renotify_interval": 0,
+		"include_tags": false,
+		"no_data_timeframe": 60,
+		"new_host_delay": 300,
+		"silenced": {}
+	},
+	"priority": null,
+	"restricted_roles": null
+}
+EOF
+}
+
+resource "datadog_monitor_json" "on_chain_kafka_offset" {
+  monitor = <<EOF
+{
+	"id": 132789224,
+	"name": "[${var.environment}] Indexer High Kafka offset lag for on-chain messages",
+	"type": "query alert",
+	"query": "min(last_10m):avg:aws.kafka.max_offset_lag{topic:to-ender AND cluster_name IN (${var.msk_cluster_name})} by {cluster_name} > 10",
+	"message": "Max. offset lag for the `to-ender` Kafka topic is > 10 meaning on-chain updates are delayed.\n\nResolution:\n- investigate why `ender` task running in ECS is not consuming from Kafka topic\n\n${local.monitor_suffix_literal}",
+	"tags": [
+		"team:${var.team}",
+		"env:${var.env_tag}"
+	],
+	"options": {
+		"thresholds": {
+			"critical": 10
+		},
+		"notify_audit": false,
+		"require_full_window": false,
+		"notify_no_data": false,
+		"renotify_interval": 0,
+		"include_tags": false,
+		"evaluation_delay": 900,
+		"new_group_delay": 0,
 		"silenced": {}
 	},
 	"priority": null,
