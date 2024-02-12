a2cfe1e3eb38a2391d73112b300bb5e3355c59b3
Piotr Święs <10480513+pswies@users.noreply.github.com>
2024-02-12 02:01:19 +0200

    Refactor Amplitude API gateway (#67)
    - Move from modules to main dir, as this is an end-deployment, not a module
    - Rename resources to match Terraform style standards (each type of resources has only one instance, so naming the instances duplicates information; therefore they can be named this, default, main or so)
    - Parametrize AWS region
    - Break down into sub-files
    - Add Terraform output item to show the effective invoke_url
    - Rename dir api_gateway to amplitude_api_gateway to express its specificity

---
+++ amplitude_api_gateway/output.tf
@@ -0,0 +1,3 @@
+output "invoke_url" {
+  value = aws_apigatewayv2_stage.this.invoke_url
+}
---
+++ amplitude_api_gateway/variables.tf
@@ -0,0 +1,5 @@
+variable "region" {
+  type        = string
+  description = "AWS region for deployment."
+  default     = "ap-northeast-1"
+}
