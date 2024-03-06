6091bf361df38081178b5e16df36064a0c9adeb3
Will Liu <will@dydx.exchange>
2024-03-06 14:15:27 -0500

    use var.environment rather than hardcode to testnet

--- modules/indexer_monitors/precautionary_monitors.tf
+++ modules/indexer_monitors/precautionary_monitors.tf
@@ -4,7 +4,7 @@ resource "datadog_monitor_json" "average_block_processing_rate" {
 	"id": 117804982,
 	"name": "[${var.environment}] Average Indexer block processing is slow",
 	"type": "query alert",
-	"query": "avg(last_5m):avg:ender.processed_block.timing.avg{env:testnet, service:indexer, success:true} / avg:dydxprotocol.blocktime_block_time_ms{env:testnet} > 0.5",
+	"query": "avg(last_5m):avg:ender.processed_block.timing.avg{env:${var.environment}, service:indexer, success:true} / avg:dydxprotocol.blocktime_block_time_ms{env:${var.environment}} > 0.5",
 	"message": "This is not an actionable alert. When this alert fires, that means that the Indexer is processing blocks slow and more time should be invested in improving Ender latency. Please notify Trading if this alert fires.\n\n${local.monitor_suffix_literal}",
 	"tags": [
 		"team:${var.team}",
@@ -35,7 +35,7 @@ resource "datadog_monitor_json" "p95_block_processing_rate" {
 	"id": 117804982,
 	"name": "[${var.environment}] p95 Indexer block processing is slow",
 	"type": "query alert",
-	"query": "avg(last_5m):avg:ender.processed_block.timing.95percentile{env:testnet, service:indexer, success:true} / avg:dydxprotocol.blocktime_block_time_ms{env:testnet} > 0.75",
+	"query": "avg(last_5m):avg:ender.processed_block.timing.95percentile{env:${var.environment}, service:indexer, success:true} / avg:dydxprotocol.blocktime_block_time_ms{env:${var.environment}} > 0.75",
 	"message": "This is not an actionable alert. When this alert fires, that means that the Indexer is processing blocks slow and more time should be invested in improving Ender latency. Please notify Trading if this alert fires.\n\n${local.monitor_suffix_literal}",
 	"tags": [
 		"team:${var.team}",
