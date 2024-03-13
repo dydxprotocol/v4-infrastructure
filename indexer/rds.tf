2e8a001f4f2918d8a0c29e449d9c328910fc7ee5
Christopher Li <Christopherfooli@gmail.com>
2024-03-13 12:34:18 -0400

    Remove multi AZ for read replicas which aren't necessary

--- indexer/rds.tf
+++ indexer/rds.tf
@@ -234,7 +234,6 @@ resource "aws_db_instance" "read_replica" {
   performance_insights_enabled          = true
   performance_insights_retention_period = 31
   auto_minor_version_upgrade            = false
-  multi_az                              = true
 
   replicate_source_db = aws_db_instance.main.identifier
 
@@ -259,7 +258,6 @@ resource "aws_db_instance" "read_replica_2" {
   performance_insights_enabled          = true
   performance_insights_retention_period = 31
   auto_minor_version_upgrade            = false
-  multi_az                              = true
 
   replicate_source_db = aws_db_instance.main.identifier
 
