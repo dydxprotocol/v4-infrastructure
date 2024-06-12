25c0fac20843f72e5de134e21915b4d8525f6400
Rudi Mocnik <17800978+rudimocnik@users.noreply.github.com>
2024-06-12 22:10:39 +0200

    replace ec2connect with ssm

--- modules/metric_ingestor/security_group.tf
+++ modules/metric_ingestor/security_group.tf
@@ -3,17 +3,6 @@ resource "aws_security_group" "main" {
   name   = "${var.environment}-${var.name}-sg"
   vpc_id = aws_vpc.main.id
 
-  # Allow port 22 for "ec2-instance-connect".
-  # This allows us to SSH into the hosts from the AWS Deveoper console.
-  # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/Connect-using-EC2-Instance-Connect.html
-  ingress {
-    protocol         = "tcp"
-    from_port        = 22
-    to_port          = 22
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
   # For outgoing traffic, allow all.
   egress {
     protocol         = "-1"
