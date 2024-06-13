bf5f44cd56f4be6334d1587d9346fd72ac5db5de
Rudi Mocnik <17800978+rudimocnik@users.noreply.github.com>
2024-06-13 21:34:33 +0200

    Remove inbound ssh security group

--- modules/metric_ingestor/security_group.tf
+++ modules/metric_ingestor/security_group.tf
@@ -3,6 +3,14 @@ resource "aws_security_group" "main" {
   name   = "${var.environment}-${var.name}-sg"
   vpc_id = aws_vpc.main.id
 
+  ingress {
+    protocol         = "-1"
+    from_port        = 0
+    to_port          = 0
+    cidr_blocks      = []
+    ipv6_cidr_blocks = []
+  }
+
   # For outgoing traffic, allow all.
   egress {
     protocol         = "-1"
