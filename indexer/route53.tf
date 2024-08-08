c1c9162762560c4785178950aaf8dbb68d36b9e9
owl-king <tranvuong0402@gmail.com>
2024-08-08 15:35:02 +0700

    Allow to enable/disable multi_az option on master node and remove read_replica_2 instance on dev* (#113)

--- indexer/route53.tf
+++ indexer/route53.tf
@@ -19,11 +19,12 @@ resource "aws_route53_record" "read_replica_1" {
 }
 
 resource "aws_route53_record" "read_replica_2" {
+  count   = var.create_read_replica_2 ? 1 : 0
   zone_id = aws_route53_zone.main.zone_id
   name    = "postgres-main-rr.dydx-indexer.private"
   type    = "CNAME"
   ttl     = "30"
-  records = ["${aws_db_instance.read_replica_2.address}"]
+  records = ["${aws_db_instance.read_replica_2[count.index].address}"]
   weighted_routing_policy {
     weight = 1
   }
