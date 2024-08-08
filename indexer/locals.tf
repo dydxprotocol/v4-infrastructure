217e9b65c8a9d8da347ac1443bee71373f1916d4
Vuong <tranvuong0402@gmail.com>
2024-08-08 17:05:00 +0700

    Reduce socks memory to 8192

--- indexer/locals.tf
+++ indexer/locals.tf
@@ -98,7 +98,7 @@ locals {
     },
     "${local.service_names["socks"]}" : {
       ecs_desired_count : 5,
-      task_definition_memory : 20480,
+      task_definition_memory : 8192,
       task_definition_cpu : 4096,
       is_public_facing : true,
       ports : [8080, 8000],
