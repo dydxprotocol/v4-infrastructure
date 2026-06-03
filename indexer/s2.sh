383cb6b88af97e2b33aa98fd365681014cb37c6a
Matt Weeks <21340974+UnbornAztecKing@users.noreply.github.com>
2026-06-03 13:14:14 -0400

    indexer: untrack local state-surgery helper scripts
    These were workflow scripts not intended for the repo.

--- indexer/s2.sh
+++
@@ -1,7 +0,0 @@
-export AWS_PROFILE=dydx-testnet
-terraform import \
-    -var environment=testnet \
-    -var datadog_api_key="$DD_API_KEY" \
-    -var datadog_app_key="$DD_APP_KEY" \
-    -var rds_db_password=stub \
-    aws_db_parameter_group.main testnet-indexer-apne1-db-parameter-group-postgres-17
